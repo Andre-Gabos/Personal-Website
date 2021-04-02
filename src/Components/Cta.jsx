130eb6de63ddc1b9396350846c6a134d5d9e84b5
André Gabos <andre.gabos@gmail.com>
2021-04-02 16:46:32 -0300

    Ajuste de nomes e estrutura de arquivos

--- src/Components/Cta.jsx
+++ src/Components/Cta.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import "../styles/style.css";
+import * as ctaStyles from "../styles/cta.module.scss";
 
-function ContactMe() {
+function Cta() {
   return (
-    <div className="contact-me">
+    <div className={ctaStyles.contactMe}>
       <h2>Get In Touch</h2>
       <h3>Talk to me abour work or cool ideas.</h3>
       <p>Use the button below if you want to send me an e-mail or click one of the links to access my profile on LinkedIn, Twitter or GitHub.</p>
-      <a className="btn" href="mailto:andre.gabos@gmail.com">CONTACT ME</a>
+      <a className={ctaStyles.btn} href="mailto:andre.gabos@gmail.com">CONTACT ME</a>
     </div>
   )
 }
 
-export default ContactMe;
+export default Cta;
