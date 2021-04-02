7e486efefbc90e97c5cae2a6d3ae66edc97b8d1d
André Gabos <andre.gabos@gmail.com>
2021-04-02 16:45:20 -0300

    Intalação do módulo de imagens do Gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,10 @@ module.exports = {
   siteMetadata: {
     title: "Gatsby-personal-website",
   },
-  plugins: ["gatsby-plugin-sass"],
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp"
+  ],
 };
