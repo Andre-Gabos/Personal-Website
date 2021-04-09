1b1fa7710f527b2e2dc020561744cf7cbbc9b0d0
André Gabos <andre.gabos@gmail.com>
2021-04-09 01:23:35 -0300

    Correção de bug na página Projects.

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Layout from "../Components/Layout";
 import Project from "../Components/Project";
+import projectInfo from "../Components/Info/project_info.js";
 
 function Projects() {
   return (
     <Layout>
       <h2>Projects</h2>
       {projectInfo
-        .filter((item, index) => index < 2)
         .map(({ id, img, title, description, ghlink, wplink }) => {
           return (
             <div>
