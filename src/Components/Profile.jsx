f6d2f8134959305badf9d2dbc3e37e79e557d8b2
André Gabos <andre.gabos@gmail.com>
2021-04-09 17:14:55 -0300

    Instalação gh-pages e alteração em imagens.

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import { StaticImage } from "gatsby-plugin-image";
 import * as profStyles from "../styles/profile.module.scss";
 
 function Profile() {
   return (
     <div className={profStyles.profile}>
-      <StaticImage src="../images/AndreCProfileCrop_200x200.jpg" alt="profile-pic" />
+      <img src="../images/AndreCProfileCrop_200x200.jpg" alt="profile-pic"></img>
       <h2>Hello.</h2>
       <p class="prof-text">I'm a Front-end Web Developer, graduated Psychologist and behavioral analyst, and my goal is to build 
       relevant software, which has the potential to contribute to the well-being and practicality of people's lives.</p>
