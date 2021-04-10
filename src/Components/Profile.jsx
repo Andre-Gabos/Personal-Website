f2a0a651a6a836a8d25edcd55eeee2f3b5ac5231
André Gabos <andre.gabos@gmail.com>
2021-04-10 20:06:11 -0300

    Correção de bugs na imagem de perfil.

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -5,7 +5,7 @@ import * as profStyles from "../styles/profile.module.scss";
 function Profile() {
   return (
     <div className={profStyles.profile}>
-      <StaticImage src="../images/AndreCProfileCrop_200x200.pgn" alt="profile picture" />
+      <StaticImage src="../images/AndreCProfileCrop_200x200.jpg" alt="profile picture" />
       <h2>Hello.</h2>
       <p class="prof-text">I'm a Front-end Web Developer, graduated Psychologist and behavioral analyst, and my goal is to build 
       relevant software, which has the potential to contribute to the well-being and practicality of people's lives.</p>
